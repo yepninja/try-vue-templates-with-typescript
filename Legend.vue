c47cac9f4a0ea6de659e016e0f07ad292ddf27ad
Petukhov Evgeniy <epetukhov@ozon.ru>
2020-02-18 20:58:13 +0300

    🎉 Initial commit

---
+++ Legend.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {Component, Emit, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class Legend extends Vue {
+        @Prop(Boolean)
+        public readonly checkbox!: boolean;
+
+        @Prop({default: () => []})
+        public legend!: string[];
+
+        private isOpen: boolean = true;
+
+        private toggle(): void {
+            this.isOpen = !this.isOpen;
+        }
+    }
+</script>
---
+++ index.vue
@@ -0,0 +1,15 @@
+<template>
+    <Legend checkbox :legend="legend" />
+</template>
+<script lang="ts">
+import {Component, Emit, Prop, Vue} from 'vue-property-decorator';
+import Legend from './Legend.vue'
+
+export default class Page extends Vue {
+
+    get legend() {
+        return 32342
+    }
+
+}
+</script>
